2306f155faaea3e298b70a36f7ac0806d75cda89
lenxeon <lenxeon@163.com>
2019-01-24 22:08:11 +0800

    go

--- project.js
+++ project.js
@@ -14,7 +14,5 @@ module.exports = {
     }, {
         name: 'index.html',
         type: 'file',
-        //   默认写入文件内容
-        content: '<!DOCTYPE html> \n<html lang="en">\n<head> \n\t<meta charset="UTF-8">\n\t<title>Title</title>\n</head>\n<body>\n</body>\n</html> '
     }]
 }
